092ccbc7275ecd461d42a8dbf0ca402d68ef4f85
Mihir Paldhikar <imihirpaldhikar@gmail.com>
2024-03-06 14:28:22 +0530

    feat: Add Navbar component.

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -21,7 +21,46 @@
  */
 
 import { JSX } from "react";
+import Link from "next/link";
 
 export default function Navbar(): JSX.Element {
-  return <nav></nav>;
+  return (
+    <nav
+      className={
+        "fixed min-w-full justify-between border-b border-b-gray-300 bg-white/50 px-5 py-3 flex items-center"
+      }
+    >
+      <div>
+        <svg
+          width="50"
+          height="50"
+          viewBox="0 0 68 68"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <rect
+            x="2.5"
+            y="2.5"
+            width="63"
+            height="63"
+            rx="7.5"
+            stroke="black"
+            strokeWidth="5"
+          />
+          <path
+            d="M24.14 53V39H28.02L33.62 48.14H31.58L37.02 39H40.9L40.94 53H36.64L36.6 45.48H37.28L33.56 51.74H31.48L27.6 45.48H28.44V53H24.14ZM43.222 53V39H49.962C51.2687 39 52.3954 39.2133 53.342 39.64C54.302 40.0667 55.042 40.68 55.562 41.48C56.082 42.2667 56.342 43.2 56.342 44.28C56.342 45.36 56.082 46.2933 55.562 47.08C55.042 47.8667 54.302 48.48 53.342 48.92C52.3954 49.3467 51.2687 49.56 49.962 49.56H45.842L47.942 47.54V53H43.222ZM47.942 48.06L45.842 45.92H49.662C50.3154 45.92 50.7954 45.7733 51.102 45.48C51.422 45.1867 51.582 44.7867 51.582 44.28C51.582 43.7733 51.422 43.3733 51.102 43.08C50.7954 42.7867 50.3154 42.64 49.662 42.64H45.842L47.942 40.5V48.06Z"
+            fill="black"
+          />
+        </svg>
+      </div>
+      <div>
+        <Link
+          href={"mailto:contact@mihirpaldhikar.com"}
+          className={"bg-blue-600 rounded-lg px-3 py-2 text-white font-medium"}
+        >
+          Contact
+        </Link>
+      </div>
+    </nav>
+  );
 }
--- src/components/Navbar/index.ts
+++ src/components/Navbar/index.ts
@@ -20,30 +20,4 @@
  * SOFTWARE.
  */
 
-import type { Metadata } from "next";
-import { Montserrat } from "next/font/google";
-import "./globals.css";
-import { ReactNode } from "react";
-import { Navbar } from "@components/Navbar";
-
-const montserrat = Montserrat({ subsets: ["latin"] });
-
-export const metadata: Metadata = {
-  title: "Mihir Paldhikar",
-  description: "Drive To Develop",
-};
-
-interface RootLayoutProps {
-  children: ReactNode;
-}
-
-export default function RootLayout({ children }: Readonly<RootLayoutProps>) {
-  return (
-    <html lang="en">
-      <body className={montserrat.className}>
-        <Navbar />
-        <main>{children}</main>
-      </body>
-    </html>
-  );
-}
+export { default as Navbar } from "./Navbar";
